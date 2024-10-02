20226b64b0de1e96f6f9d556335af90cbff4560d
minhngonmcoding <minhngon.dang1@gmail.com>
2024-10-02 21:53:44 +0700

    Update convert_to_yolo.py
    Convert data to Yolo format

--- src/utils/convert_to_yolo.py
+++ src/utils/convert_to_yolo.py
@@ -2,6 +2,7 @@ import json
 import os
 from PIL import Image
 
+
 def convert_to_yolo(task_output, image_width, image_height, class_id=0):
     yolo_annotations = []
 
@@ -55,7 +56,8 @@ import random
 
 def create_train_valid_txt(image_folder, train_split=0.8):
     # Get a list of all image filenames in the folder
-    image_fnames = [f for f in os.listdir(image_folder) if os.path.isfile(os.path.join(image_folder, f))]
+    image_fnames = [f for f in os.listdir(image_folder) if
+                    f.endswith('.jpg') and os.path.isfile(os.path.join(image_folder, f))]
 
     # Shuffle the images to ensure random distribution
     random.shuffle(image_fnames)
@@ -73,48 +75,68 @@ def create_train_valid_txt(image_folder, train_split=0.8):
 
     # Write the training images to train.txt
     with open(train_txt_path, "w") as f:
-        for image_fname in train_images:
-            f.write(image_fname + "\n")
+        for index, image_fname in enumerate(train_images):
+            if index == len(train_images) - 1:
+                f.write(image_folder+ image_fname)
+            else:
+                f.write(image_folder+ image_fname + "\n")
 
     # Write the validation images to valid.txt
     with open(valid_txt_path, "w") as f:
-        for image_fname in valid_images:
-            f.write(image_fname + "\n")
+        for index, image_fname in enumerate(valid_images):
+            if index == len(valid_images) - 1:
+                f.write(image_folder+ image_fname)
+            else:
+                f.write(image_folder+ image_fname + "\n")
 
 
 # Usage example:
 # create_train_valid_txt("/path/to/your/image/folder")
 
+def create_yolo_folder(image_folder, json_folder, yolo_folder):
+    for json_fname in os.listdir(json_folder):
+        file_path = os.path.join(json_folder, json_fname)
+        image_found = False
+        base_name = os.path.splitext(json_fname)[0]
+        image_fname = base_name + ".jpg"
+        image_path = os.path.join(image_folder, image_fname)
+
+        if not os.path.exists(yolo_folder):
+            os.makedirs(yolo_folder)
+        yolo_path = os.path.join(yolo_folder, base_name+".txt")
+
+        if os.path.exists(image_path):
+            image_found = True
+            with Image.open(image_path) as img:
+                width, height = img.size
+                print(f"Image {base_name} has height: {height} and width: {width}")
+
+        if not image_found:
+            print(f"No corresponding image found for {base_name}.")
+            continue
+
+        with open(file_path, "r") as f:
+            data = json.load(f)
+            annotation_data = convert_to_yolo(data["task2"]["output"], image_width=width, image_height=height)
+
+            with open(yolo_path, "w") as f:
+                for annotate in annotation_data:
+                    f.write(annotate + "\n")
+
 
 if __name__ == "__main__":
-    json_folder = "../../data/train/final_data/json"
-    image_folder = "../../data/train/final_data/images"
-    yolo_folder = "../../data/train/final_data/yolo"
+    json_folder = "../../data/raw_data/train/final_data/json"
+    image_folder = "../../data/raw_data/train/final_data/images"
+    yolo_folder = "../../data/raw_data/train/final_data/yolo"
 
-    create_train_valid_txt(image_folder)
-    # for json_fname in os.listdir(json_folder):
-    #     file_path = os.path.join(json_folder, json_fname)
-    #     image_found = False
-    #     base_name = os.path.splitext(json_fname)[0]
-    #     image_fname = base_name + ".jpg"
-    #     image_path = os.path.join(image_folder, image_fname)
-    #     yolo_path = os.path.join(yolo_folder, base_name+".txt")
-    #
-    #     if os.path.exists(image_path):
-    #         image_found = True
-    #         with Image.open(image_path) as img:
-    #             width, height = img.size
-    #             print(f"Image {base_name} has height: {height} and width: {width}")
-    #
-    #     if not image_found:
-    #         print(f"No corresponding image found for {base_name}.")
-    #         continue
-    #
-    #
-    #     with open(file_path, "r") as f:
-    #         data = json.load(f)
-    #         annotation_data = convert_to_yolo(data["task2"]["output"], image_width=width, image_height=height)
-    #
-    #         with open(yolo_path, "w") as f:
-    #             for annotate in annotation_data:
-    #                 f.write(annotate + "\n")
+    test_image_folder = "../../data/test/chart_images/split_2/images"
+    test_json = "../../data/test/final_full_GT/split_2/annotations_JSON"
+    yolo_test_folder = "../../data/test/final_full_GT/split_2/yolo"
+
+    # create_yolo_folder(test_image_folder, test_json, yolo_test_folder)
+
+    create_train_valid_txt(image_folder="../../data/darknet_rotated/train/",
+                           train_split=0.8)
+
+
+    # create_train_valid_txt(image_folder)
